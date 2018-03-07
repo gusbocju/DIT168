3885c8ab92fc449bf0cd5e0f70ea7d3e37707e33
gusbocju <gusbocju@student.gu.se>
2018-03-07 12:21:54 +0100

    some refactoring, hoping for better results

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,11 +15,11 @@ int main(int /*argc*/, char **/*argv*/) {
      internal = new cluon::OD4Session(111, [](cluon::data::Envelope &&envelope) noexcept {
         if (envelope.dataType() == opendlv::proxy::GroundSteeringReading::ID()) {
             opendlv::proxy::GroundSteeringReading receivedMsg = cluon::extractMessage<opendlv::proxy::GroundSteeringReading>(std::move(envelope));
-            std::cout << "GroundSteering_msg sent! " << receivedMsg.groundSteering() << std::endl;
+            std::cout << "GroundSteering sent! " << receivedMsg.groundSteering() << std::endl;
         }
         else if (envelope.dataType() == opendlv::proxy::PedalPositionReading::ID()) {
             opendlv::proxy::PedalPositionReading receivedMsg = cluon::extractMessage<opendlv::proxy::PedalPositionReading>(std::move(envelope));
-            std::cout << "PedalPosition_msg sent!" << receivedMsg.position() << std::endl;
+            std::cout << "PedalPosition sent!" << receivedMsg.position() << std::endl;
         }
     });
 
@@ -29,8 +29,8 @@ int main(int /*argc*/, char **/*argv*/) {
             SteeringInstruction ins = cluon::extractMessage<SteeringInstruction>(std::move(envelope));
             opendlv::proxy::GroundSteeringReading msgSteering;
             opendlv::proxy::PedalPositionReading msgPedal;
-            msgSteering.groundSteering(ins.steeringAngle() -45);
-            msgPedal.position(ins.pedalPosition() /100);
+            msgSteering.groundSteering(ins.steeringAngle());
+            msgPedal.position(ins.pedalPosition());
             internal->send(msgSteering);
             internal->send(msgPedal);
         }
@@ -38,10 +38,10 @@ int main(int /*argc*/, char **/*argv*/) {
 
     while (1) {
         std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-        if (now.time_since_epoch().count() -last_instruction_timestamp > 500) {
+        if (now.time_since_epoch().count() -last_instruction_timestamp > 5000) {
             SteeringInstruction brake;
             brake.pedalPosition(0);
-            brake.steeringAngle(45);
+            brake.steeringAngle(0);
             external->send(brake);
         }
         last_instruction_timestamp = now.time_since_epoch().count();
