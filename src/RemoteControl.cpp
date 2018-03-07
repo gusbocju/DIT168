ec28c3beec4d884d3a0f404aa4ef2fd9b9c15dc1
gusbocju <gusbocju@student.gu.se>
2018-03-07 13:49:55 +0100

    fixed an issue with the RemoteControl sending too many (brake) instructions

--- src/RemoteControl.cpp
+++ src/RemoteControl.cpp
@@ -8,16 +8,17 @@
 
 int main(int /*argc*/, char** /*argv*/) {
     cluon::OD4Session od4(112, [](cluon::data::Envelope /*&&envelope*/) noexcept {});
-
     bool fwd;
     while (1) {
         SteeringInstruction ins;
-        switch (std::getchar()) {
+        std::string input;
+        std::getline(std::cin, input);
+        switch (input.at(0)) {
             case 'w': ins.pedalPosition(0.5); ins.steeringAngle(0); fwd = true; break;
             case 'a': ins.pedalPosition(fwd ? 0.25 : -0.25); ins.steeringAngle(-45); break;
             case 's': ins.pedalPosition(-0.5); ins.steeringAngle(0); fwd = false; break;
             case 'd': ins.pedalPosition(fwd ? 0.25 : -0.25); ins.steeringAngle(45); break;
-            default:  ins.pedalPosition(0); ins.steeringAngle(0); break;
+            default: ins.pedalPosition(0); ins.steeringAngle(0);
         }
         std::cout << "sending " << ins.pedalPosition() << " / " << ins.steeringAngle() << " ..." << std::endl;
         od4.send(ins);
