58b450c3cbe1deff45eb99a33d4c5e8ab01a84f1
Julian Bock <gusbocju@student.gu.se>
2018-04-11 13:22:31 +0200

    first attempt on handling controller events

--- src/ps4controller/demo/Main.cpp
+++ src/ps4controller/demo/Main.cpp
@@ -7,12 +7,32 @@
 #include <fstream>
 #include <unistd.h>
 
+typedef struct {
+    char timestamp_b0;
+    char timestamp_b1;
+    char timestamp_b2;
+    char timestamp_b3;
+    char data_b0;
+    char data_b1;
+    char type;
+    char id;
+} PS4ControllerEvent;
+
 int main()
 {
-    while (1) {
-        std::ifstream ifs("/dev/input/js0");
-        if (ifs.is_open())
-            std::cout << ifs.rdbuf() << std::endl;
-        usleep(1000);
+    while (true) {
+        FILE *file = fopen("/dev/input/js0", "rb");
+        fseek(file, 0L, SEEK_END);
+        long size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+        if (size >= 7) {
+            PS4ControllerEvent *event = (PS4ControllerEvent *) malloc(sizeof(PS4ControllerEvent));
+            while (!feof(file)) {
+                if (fread(event, sizeof(PS4ControllerEvent), 1, file))
+                    std::cout << "event.Type: " << event->type << " / event.Id: " << event->id << std::endl;
+            }
+            usleep(1000);
+        }
+
     }
 }
