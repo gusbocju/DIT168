cdcfd3cac746ccd9d274a35a244f45a04ffe95f6
Julian Bock <gusbocju@student.gu.se>
2018-05-03 11:58:48 +0200

    added fixed-turning options to the DS4 microservice

--- src/DS4/DS4RemoteControl.cpp
+++ src/DS4/DS4RemoteControl.cpp
@@ -12,9 +12,9 @@ int main(int argc, char** argv)
         0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("safety-distance")) {
         std::cerr << argv[0] << " reads (wireless) controller input from the specified path and relays it to components on the BeagleBone."
                   << std::endl;
-        std::cerr << "Usage:   " << argv[0] << " --dev=<path toController> --freq=<int pollingRate> --cid=<OD4Session toComponents> --safety-distance=<int cm> --fixed_speed=<(optional)>"
+        std::cerr << "Usage:   " << argv[0] << " --dev=<path toController> --freq=<int pollingRate> --cid=<OD4Session toComponents> --safety-distance=<int cm> --fixed-speed=<(optional)> --fixed-turning=<(optional)>"
                   << std::endl;
-        std::cerr << "Example: " << argv[0] << " --dev=/dev/input/js0 --freq=50 --cid=111 --safety-distance=20 --fixed_speed=0.12" << std::endl;
+        std::cerr << "Example: " << argv[0] << " --dev=/dev/input/js0 --freq=50 --cid=111 --safety-distance=20 --fixed-speed=0.12 --fixed-turning=0.45" << std::endl;
         retVal = 1;
     } else {
         const std::string DEV = commandlineArguments["dev"];
@@ -27,7 +27,12 @@ int main(int argc, char** argv)
         float steering = 0.f, steeringCorrection = 0.f, speed = 0.f, speedCorrection = 0.f;
 
         float FIXED_SPEED = 0;
-        if (0 != commandlineArguments.count("fixed_speed")) FIXED_SPEED = std::stof(commandlineArguments["fixed_speed"]);
+        if (0 != commandlineArguments.count("fixed-speed"))
+            FIXED_SPEED = std::stof(commandlineArguments["fixed-speed"]);
+
+        float FIXED_TURNING = 0;
+        if (0 != commandlineArguments.count("fixed-turning"))
+            FIXED_TURNING = std::stof(commandlineArguments["fixed-turning"]);
 
         std::shared_ptr<cluon::OD4Session> od4 = std::make_shared<cluon::OD4Session>(CID,
         [&od4, &distance, &speedCorrection, &steeringCorrection, &steering, &correctionEnabled](cluon::data::Envelope &&envelope) noexcept {
@@ -118,6 +123,8 @@ int main(int argc, char** argv)
                             switch (event->id) {
                                 case LStickX: {
                                     steering = absToPercentage(event->data)*(-1);
+                                    if (FIXED_TURNING && steering != 0)
+                                        steering = steering > 0 ? FIXED_TURNING : -FIXED_TURNING;
                                     MARBLE::Steering::Instruction::GroundSteering steeringInstruction;
                                     steeringInstruction.groundSteering(steering);
                                     od4->send(steeringInstruction);
